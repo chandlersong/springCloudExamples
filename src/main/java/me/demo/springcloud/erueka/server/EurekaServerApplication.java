30844ff7101117283e38bf1c0c97b65e26f08020
chandlersong <chandler605@gmail.com>
2018-07-22 22:12:59 +0800

    limit spring boot's application bean search

--- src/main/java/me/demo/springcloud/erueka/server/EurekaServerApplication.java
+++ src/main/java/me/demo/springcloud/erueka/server/EurekaServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("me.demo.springcloud.erueka.server")
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
