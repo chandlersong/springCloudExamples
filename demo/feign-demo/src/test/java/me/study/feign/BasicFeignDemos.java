c352244ba4981d9423f957b03c6c2ef23757ca4d
chandlersong <chandler605@gmail.com>
2018-10-06 21:40:29 +0800

    simple feign demo added

--- demo/feign-demo/src/test/java/me/study/feign/BasicFeignDemos.java
+++ demo/feign-demo/src/test/java/me/study/feign/BasicFeignDemos.java
@@ -1,9 +1,7 @@
 /*
- * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
+ * Copyright (c) 2018
+ * @Author:chandler@yeah.net
+ * @LastModified:2018-10-06T21:40:28.026+08:00
  */
 
 package me.study.feign;
