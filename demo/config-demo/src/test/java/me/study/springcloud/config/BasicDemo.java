28b4b3fd8f111aec386caf1aeaa293397ff2f025
chandlersong <chandler605@gmail.com>
2018-10-11 22:54:55 +0800

    how to use local repository

--- demo/config-demo/src/test/java/me/study/springcloud/config/BasicDemo.java
+++ demo/config-demo/src/test/java/me/study/springcloud/config/BasicDemo.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2018
  * @Author:chandler song, email:chandler605@outlook.com
- * @LastModified:2018-10-11T07:16:56.913+08:00
+ * @LastModified:2018-10-11T22:54:54.759+08:00
  * LGPL licence
  *
  */
@@ -28,6 +28,21 @@ public class BasicDemo {
     public void testHelloWorld() {
 
         ServerRunner.createAndRunServer(SimpleConfigServer.class, "simple_config_server_without_eureka.yml");
+        ServerRunner.createAndRunServer(SimpleConfigClient.class, "simple_config_client_without_eureka.yml", "simple_config_client_bootstrap-default.yml");
+        ServerRunner.createAndRunServer(SimpleConfigClient.class, "simple_config_client_without_eureka.yml", "simple_config_client_bootstrap-dev1.yml");
+        ServerRunner.createAndRunServer(SimpleConfigClient.class, "simple_config_client_without_eureka.yml", "simple_config_client_bootstrap-dev2.yml");
+
+        Assert.assertEquals("default", RestRequest.get(8081, ""));
+        Assert.assertEquals("dev", RestRequest.get(8082, ""));
+        Assert.assertEquals("dev", RestRequest.get(8083, ""));
+        logger.info("stop");
+    }
+
+
+    @Test
+    public void testLocalRepo() {
+
+        ServerRunner.createAndRunServer(SimpleConfigServer.class, "local_config_server_without_eureka.yml");
         ServerRunner.createAndRunServer(SimpleConfigClient.class, "simple_config_client_without_eureka.yml", "simple_config_client_bootstrap-default.yml");
         ServerRunner.createAndRunServer(SimpleConfigClient.class, "simple_config_client_without_eureka.yml", "simple_config_client_bootstrap-dev1.yml");
         ServerRunner.createAndRunServer(SimpleConfigClient.class, "simple_config_client_without_eureka.yml", "simple_config_client_bootstrap-dev2.yml");
