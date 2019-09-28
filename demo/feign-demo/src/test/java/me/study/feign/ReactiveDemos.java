a129e38e2d9c27d63f11dbd96d8eecc2902d96d8
chandlersong <chandler605@gmail.com>
2019-09-29 06:26:31 +0800

    feign: make reactive code can run in one ut

--- demo/feign-demo/src/test/java/me/study/feign/ReactiveDemos.java
+++ demo/feign-demo/src/test/java/me/study/feign/ReactiveDemos.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2019
  * @Author:chandler song, email:chandler605@outlook.com
- * @LastModified:2019-09-28T23:01:12.934+08:00
+ * @LastModified:2019-09-29T06:26:30.011+08:00
  * LGPL licence
  *
  */
@@ -11,6 +11,7 @@ package me.study.feign;
 import lombok.extern.slf4j.Slf4j;
 import me.demo.springcloud.utils.RestRequest;
 import me.demo.springcloud.utils.ServerRunner;
+import me.study.reactivefeign.ReactiveFeignApplication;
 import me.study.springcloud.eureka.server.EurekaServerApplication;
 import me.study.springcloud.services.ok.ReactiveOKServiceApplication;
 import org.junit.Test;
@@ -19,18 +20,19 @@ import org.junit.Test;
 public class ReactiveDemos {
 
     @Test
-    public void testHelloWorld() {
+    public void testHelloWorld() throws InterruptedException {
         log.info("server port:{}", "8080");
-        /**
-         * run the application manually， and add a break point at request line
-         */
-        //ServerRunner.createAndRunServer(ReactiveFeignApplication.class, "reactive/application.yml");
-        ServerRunner.createAndRunServer(EurekaServerApplication.class, "simplest_eureka_server.yml");
-        ServerRunner.createAndRunServer(ReactiveOKServiceApplication.class, "reactive/reactive_ok_services_client.yml");
 
-
+        startServer();
         String response = RestRequest.get("/greeting");
         log.info("request get:,{}", response);
         log.info("stop");
+    }
+
+    private void startServer() throws InterruptedException {
+        ServerRunner.createAndRunServer(EurekaServerApplication.class, "simplest_eureka_server.yml");
+        ServerRunner.createAndRunServer(ReactiveFeignApplication.class, "reactive/application.yml");
+        ServerRunner.createAndRunServer(ReactiveOKServiceApplication.class, "reactive/reactive_ok_services_client.yml");
+        Thread.sleep(60 * 1000);
     }
 }
