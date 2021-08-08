bf54af6ecf7b2ba4521912d4b614370d8bf421d6
chandlersong <chandler605@gmail.com>
2021-08-08 22:08:37 +0800

    spring cloud connect: add example how to use official table

--- demo/stream-demo/kafka-demo/env/simple-kafka-demo/db/initial/db_initial.sql
+++ demo/stream-demo/kafka-demo/env/simple-kafka-demo/db/initial/db_initial.sql
@@ -1,3 +1,4 @@
+drop table if exists user;
 CREATE TABLE user
 (
     id              INT          NOT NULL AUTO_INCREMENT,
@@ -6,3 +7,14 @@ CREATE TABLE user
     favorite_color  VARCHAR(100),
     PRIMARY KEY (id)
 );
+
+drop table if exists test_avroTopic;
+CREATE TABLE test_avroTopic
+(
+    id              INT          NOT NULL AUTO_INCREMENT,
+    name            VARCHAR(100) NOT NULL,
+    favorite_number int          NOT NULL,
+    favorite_color  VARCHAR(100),
+    PRIMARY KEY (id)
+);
+
